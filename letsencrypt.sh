0a627325925776d0c6766d3271f295c25a2b8be6
ImLvna <44179559+ImLvna@users.noreply.github.com>
2021-04-15 14:21:39 -0400

    Update letsencrypt.sh

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo yes yum install python3 python3-venv libaugeas0
+yes | sudo yum install python3 python3-venv libaugeas0
 sudo python3 -m venv /opt/certbot/
 sudo /opt/certbot/bin/pip install --upgrade pip
 sudo /opt/certbot/bin/pip install certbot
