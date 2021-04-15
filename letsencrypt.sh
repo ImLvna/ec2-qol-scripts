d5b85e8b3e59a6ff70cccdfdaeb1104a407418e9
ImLvna <44179559+ImLvna@users.noreply.github.com>
2021-04-15 14:18:34 -0400

    Create letsencrypt.sh

---
+++ letsencrypt.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo yes yum install python3 python3-venv libaugeas0
+sudo python3 -m venv /opt/certbot/
+sudo /opt/certbot/bin/pip install --upgrade pip
+sudo /opt/certbot/bin/pip install certbot
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+sudo certbot certonly --standalone
